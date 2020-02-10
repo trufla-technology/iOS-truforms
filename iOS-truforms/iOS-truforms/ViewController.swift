91c395f03f1efbb35a6f00d77aed4bf2cdc3920d
mustafa.m@trufla.com <mezzat@waqood.sa>
2020-02-10 12:42:17 +0200

    Project init

---
+++ iOS-truforms/iOS-truforms/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-truforms
+//
+//  Created by Trufla on 2/10/20.
+//  Copyright © 2020 Trufla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
