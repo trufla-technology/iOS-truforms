c447e1f4159f76e49675551855ad54b6d8590a04
Mustafa <mezzat@waqood.sa>
2020-04-30 17:13:38 +0200

    Url done

--- iOS-truforms/iOS-truforms/GenericModule/EurekaManager/EurekaManager.swift
+++ iOS-truforms/iOS-truforms/GenericModule/EurekaManager/EurekaManager.swift
@@ -17,13 +17,13 @@ protocol EurekaManagerDelegate: class {
     
     func addRow <R:BaseRow> (_ row:R, at tag: String)
     
-    func insertSection()
+    func insertSection(_ childTag: String)
 }
 
 class EurekaManager {
     weak var delegate:EurekaManagerDelegate!
     var ignoreTitle: Bool = true
-    
+        
     func draw(_ node: SchemaObjectProtocol) {
         if let node = node as? SchemaObject {
             drawObject(node)
@@ -41,29 +41,78 @@ class EurekaManager {
             drawString(node)
         }
     }
+    
+    private func detectRaw(_ node: SchemaObjectProtocol) -> BaseRow {
+        if let node = node as? SchemaObject {
+            return createLabelRaw(node)
+        }
+        if let node = node as? SchemaEnum {
+            return detectGenericPickerData(node)
+        }
+        if let node = node as? SchemaEnumData {
+            return createLabelRaw(node)
+        }
+        if let node = node as? SchemaString {
+            return detectStringRaw(node)
+        }
+        return createLabelRaw(node)
+    }
+    
+    private func detectGenericPickerData(_ node: SchemaEnum) -> BaseRow {
+        let items = node.itemsToDisplay()
+        if items is [String] {
+            let pickerInputRow: PickerInputRow<String> = createPickerInputRow(node)
+            return pickerInputRow
+        }
+        let pickerInputRow: PickerInputRow<Int> = createPickerInputRow(node)
+        return pickerInputRow
+    }
+    
+    private func detectStringRaw(_ node: SchemaString) -> BaseRow{
+         let formats = SchemaNodeConstants.StringFormats.self
+         switch node.format {
+         case formats.DATE:
+             return createDateRow(node)
+         case formats.DATETIME:
+             return createDateTimeRow(node)
+         case formats.EMAIL:
+            return createEmailRow(node)
+         case formats.MAPLOCATION:
+            return createLocationRaw(node)
+         case formats.PHONE:
+             return createPhoneRow(node)
+         case formats.PHOTO:
+             return createPhotoRow(node)
+         case formats.TIME:
+             return createTimeRow(node)
+         default:
+             return createTextFieldRow(node)
+         }
+     }
         
     private func drawObject(_ node: SchemaObject) {
         delegate.addSection(title: node.title(), with: node.tag, at: node.parentTag, ignoreTitle: ignoreTitle)
         ignoreTitle = false
     }
+    
     private func drawArray(_ node: SchemaArray) {
-        delegate.insertSection()
-//        drawObject(objprotocol)
+        delegate.insertSection(node.tag)
+    }
+    private func createPickerInputRow<T: Equatable>(_ node: SchemaEnum) -> PickerInputRow<T> {
+        let items = node.itemsToDisplay()
+        let pickerInputRow = PickerInputRow<T>()
+        pickerInputRow.title = node.title()
+        pickerInputRow.options = items as! [T]
+        return pickerInputRow
     }
     private func drawEnum(_ node: SchemaEnum) {
         let items = node.itemsToDisplay()
-        print(items)
         if items is [String] {
-            let pickerInputRow = PickerInputRow<String>()
-            pickerInputRow.title = node.title()
-            pickerInputRow.options = items as! [String]
+            let pickerInputRow: PickerInputRow<String> = createPickerInputRow(node)
             delegate.addRow(pickerInputRow, at: node.parentTag)
         }
-        
         if items is [Int] {
-            let pickerInputRow = PickerInputRow<Int>()
-            pickerInputRow.title = node.title()
-            pickerInputRow.options = items as! [Int]
+            let pickerInputRow: PickerInputRow<Int> = createPickerInputRow(node)
             delegate.addRow(pickerInputRow, at: node.parentTag)
         }
     }
@@ -91,54 +140,89 @@ class EurekaManager {
             drawTextField(node)
         }
     }
-    private func drawTextField(_ node: SchemaString) {
-        //        print("Text area: ", node.title())
+    private func createTextFieldRow(_ node: SchemaObjectProtocol) -> TextRow {
         let textRow = TextRow()
         textRow.title = node.title()
         textRow.placeholder = node.title()
-        
+        return textRow
+    }
+    private func drawTextField(_ node: SchemaString) {
+        let textRow = createTextFieldRow(node)
         delegate.addRow(textRow, at: node.parentTag)
     }
+    private func createDateRow(_ node: SchemaObjectProtocol) -> DateRow {
+         let dateRow = DateRow()
+         dateRow.title = node.title()
+         dateRow.value = Date(timeIntervalSinceReferenceDate: 0)
+         return dateRow
+    }
     private func drawDate(_ node: SchemaString) {
-        let dateRow = DateRow()
-        
-        dateRow.title = node.title()
-        dateRow.value = Date(timeIntervalSinceReferenceDate: 0)
-        
+        let dateRow = createDateRow(node)
         delegate.addRow(dateRow, at: node.parentTag)
     }
+    private func createDateTimeRow(_ node: SchemaObjectProtocol) -> DateTimeRow {
+         let dateTimeRow = DateTimeRow()
+         dateTimeRow.title = node.title()
+         return dateTimeRow
+    }
     private func drawDateTime(_ node: SchemaString) {
-        let dateTimeRow = DateTimeRow()
-        dateTimeRow.title = node.title()
-        
+        let dateTimeRow = createDateTimeRow(node)
         delegate.addRow(dateTimeRow, at: node.parentTag)
     }
-    private func drawTime(_ node: SchemaString) {
+    private func createTimeRow(_ node: SchemaObjectProtocol) -> TimeRow {
         let timeRow = TimeRow()
         timeRow.title = node.title()
-        
+        return timeRow
+    }
+    private func drawTime(_ node: SchemaString) {
+        let timeRow = createTimeRow(node)
         delegate.addRow(timeRow, at: node.parentTag)
     }
-    private func drawEmail(_ node: SchemaString) {
+    private func createEmailRow(_ node: SchemaObjectProtocol) -> EmailRow {
         let emailRow = EmailRow()
         emailRow.title = node.title()
         emailRow.placeholder = node.title()
+        return emailRow
+    }
+    private func drawEmail(_ node: SchemaString) {
+        let emailRow = createEmailRow(node)
         delegate.addRow(emailRow, at: node.parentTag)
     }
-    private func drawPhone(_ node: SchemaString) {
+    private func createPhoneRow(_ node: SchemaObjectProtocol) -> PhoneRow {
         let phoneRow = PhoneRow()
         phoneRow.title = node.title()
         phoneRow.placeholder = node.title()
+        return phoneRow
+    }
+    private func drawPhone(_ node: SchemaString) {
+        let phoneRow = createPhoneRow(node)
         delegate.addRow(phoneRow, at: node.parentTag)
     }
-    private func drawPhoto(_ node: SchemaString) {
+    private func createPhotoRow(_ node: SchemaObjectProtocol) -> ImageRow {
         let imageRow = ImageRow()
         imageRow.title = node.title()
         imageRow.sourceTypes = [.PhotoLibrary, .SavedPhotosAlbum]
         imageRow.clearAction = .yes(style: UIAlertAction.Style.destructive)
+        return imageRow
+    }
+    private func drawPhoto(_ node: SchemaString) {
+        let imageRow = createPhotoRow(node)
         delegate.addRow(imageRow, at: node.parentTag)
     }
+    private func createLabelRaw(_ node: SchemaObjectProtocol) -> LabelRow {
+        let labelRow = LabelRow()
+        labelRow.title = node.title()
+        return labelRow
+    }
+    private func drawLabel(_ node: SchemaObject) {
+        let labelRow = createLabelRaw(node)
+        delegate.addRow(labelRow, at: node.parentTag)
+    }
+    private func createLocationRaw(_ node: SchemaObjectProtocol) -> LocationRow {
+        let locationRow = LocationRow()
+        locationRow.title = node.title()
+        return locationRow
+    }
     private func drawMapLocation(_ node: SchemaString) {
-        
     }
 }
--- iOS-truforms/iOS-truforms/Module/View/SchemaNodeViewController.swift
+++ iOS-truforms/iOS-truforms/Module/View/SchemaNodeViewController.swift
@@ -23,7 +23,6 @@ class SchemaNodeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
         manager = EurekaManager()
         manager?.delegate = self
         let module = SchemaNodeConfig()
@@ -57,18 +56,7 @@ extension SchemaNodeViewController: EurekaManagerDelegate {
     func addRow <R:BaseRow> (_ row:R, at tag: String)  {
         form.sectionBy(tag: tag)! <<< row
     }
-    
-    func addPicker(title:String) {
-        form +++ PickerInputRow<String>("Picker Input Row"){
-            $0.title = "Options"
-            $0.options = []
-            for i in 1...10{
-                $0.options.append("option \(i)")
-            }
-            $0.value = $0.options.first
-        }
-    }
-    
+
     func addSection(title:String, with tag: String, at parentTag:String, ignoreTitle: Bool)  {
         var sectionTitle = ""
         if !ignoreTitle {
@@ -77,79 +65,8 @@ extension SchemaNodeViewController: EurekaManagerDelegate {
         let section = Section(sectionTitle)
         section.tag = tag
         form +++ section
-        
-        /*if let parentSection = form.sectionBy(tag: parentTag) {
-         parentSection +++ section
-         } else {
-         }*/
     }
     
-    
-  
-    func insertSection()  {
-        form +++  ButtonRow() {
-            $0.title = "Add"
-            $0.cellStyle = .value1
-        }.cellUpdate { cell, _ in
-            cell.textLabel?.textAlignment = .left
-            cell.imageView?.image = UIImage(named:"plus_image")
-            
-        }.onCellSelection { cell, row in
-            let dynamicSection = Section("Dynamic Section")
-                    <<< TextRow(){ row in
-                        row.title = "Text Row"
-                        row.placeholder = "Enter text here"
-                    }
-                    <<< PhoneRow(){
-                        $0.title = "Phone Row"
-                        $0.placeholder = "And numbers here"
-                    }
-                self.form.insert(dynamicSection, at: (row.indexPath?.section)!)
-            }
-        }
-        
-        
+    func insertSection(_ tag: String)  {
     }
-    func addMapLocation()  {
-        //       form +++ LocationRow(){
-        //            $0.title = "Location"
-        //            $0.value = CLLocation(latitude: latitude, longitude: longitude)
-        //            $0.tag = "location"
-        //            $0.validationOptions = .validatesOnChange //2
-        //            $0.cellUpdate { (cell, row) in //3
-        //                if !row.isValid {
-        //                    cell.textLabel?.textColor = .red
-        //                } else {
-        //                    let lastLocation = row.value
-        //                    let geocoder = CLGeocoder()
-        //                    geocoder.reverseGeocodeLocation(lastLocation!,
-        //                        completionHandler: { (placemarks, error) in
-        //                            if error == nil {
-        //                                let place = placemarks![0]
-        //                                var adressString : String = ""
-        //                                if place.thoroughfare != nil {
-        //                                    adressString = adressString + place.thoroughfare! + ", "
-        //                                }
-        //                                if place.subThoroughfare != nil {
-        //                                    adressString = adressString + place.subThoroughfare! + " "
-        //                                }
-        //                                if place.locality != nil {
-        //                                    adressString = adressString + place.locality! + " - "
-        //                                }
-        //                                if place.postalCode != nil {
-        //                                    adressString = adressString + place.postalCode! + " "
-        //                                }
-        //                                if place.subAdministrativeArea != nil {
-        //                                    adressString = adressString + place.subAdministrativeArea! + " - "
-        //                                }
-        //                                if place.country != nil {
-        //                                    adressString = adressString + place.country!
-        //                                }
-        //
-        //                                self.address = adressString.trimmingCharacters(in: .whitespacesAndNewlines)
-        //                            }
-        //                    })
-        //                }
-        //            }
-        //        }
-    }
+}
